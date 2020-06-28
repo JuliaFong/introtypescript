184a76a17e3076d6b4c6fd30e0c629eaa5d9e92b
Julia Fong <Juliamzfong@gmail.com>
2020-06-28 15:41:49 -0700

    Debug

--- index.ts
+++ index.ts
@@ -3,6 +3,21 @@ import axios from 'axios';
 const url = "https://jsonplaceholder.typicode.com/todos/1"
 
 axios.get(url).then(response => {
-    console.log(response.data)
+    interface ToDo {
+        id: number;
+        title: string;
+        completed: boolean;
+    }
+    const todo = response.data as ToDo
+
+    const id = todo.id
+    const title = todo.title
+    const completed = todo.completed
+
+    console.log(`
+    The Todo with ID: ${id}
+    Has a title of: ${title}
+    Is it finished? ${completed}
+    `);
 });
 

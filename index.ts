e891180cd2fe0c8da4ee038ea9b57b2706a8b6df
Julia Fong <Juliamzfong@gmail.com>
2020-06-28 22:01:52 -0700

    Add notes

--- index.ts
+++ index.ts
@@ -28,3 +28,21 @@ const logTodo = (id: number, title: string, completed: boolean) => {
     `);
 };
 
+
+//typescript has primitive types and object types
+
+// primitive tiypes include number, boolean, void undefined
+//string, symbol. null
+
+//object types include functions, arrays, classes, and objects
+ 
+//why are there two types?
+
+//Type: easy way to refer to different properties + functions that a value has
+
+// we care about types because Types are used by the
+//TypeScript Compiler to analyze our code for errors
+
+//Types allow other engineers to understand what
+// values are flowing around our codebase
+
